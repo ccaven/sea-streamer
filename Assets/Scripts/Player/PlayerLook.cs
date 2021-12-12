70a696e8ba7166f0334d636fbdae87514603e884
xacer <chacecaven@outlook.com>
2021-12-12 16:36:00 -0500

    Added sample scene and player camera controller

---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+    public static PlayerLook instance;
+
+    [Header("References")]
+    public Transform cameraHolder;
+    public Transform playerHead;
+
+    [Header("Settings")]
+    public float sensitivity;
+    public float maxPitch;
+
+    private float pitch;
+    private float yaw;
+
+    private Vector3 upVector;
+
+    private Quaternion GetRotation() {
+        Vector3 lookDirection = Quaternion.Euler(pitch, yaw, 0f) * Vector3.forward;
+        return Quaternion.LookRotation(lookDirection, upVector);
+    }
+
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update() {
+
+        if (Cursor.lockState != CursorLockMode.Locked && Input.GetMouseButton(0)) {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        if (Cursor.lockState == CursorLockMode.Locked && Input.GetKey(KeyCode.Escape)) {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        // Update pitch and yaw
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sensitivity;
+
+        yaw += mouseX * Time.deltaTime;
+        pitch -= mouseY * Time.deltaTime;
+
+        pitch = Mathf.Clamp(pitch, -maxPitch, maxPitch);
+
+        // Update camera's position and rotation
+        cameraHolder.rotation = GetRotation();
+        cameraHolder.position = playerHead.position;
+    }
+
+    private void Awake() {
+        instance = this;
+
+        upVector = Vector3.up;
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public static PlayerMovement instance;
+
+    private void Awake() {
+        instance = this;
+    }
+}
